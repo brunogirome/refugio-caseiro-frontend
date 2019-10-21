82b5ac952ab723c73fb9d7967c802f5c10829889
Bruno Girome Oliveira <28264088@sempreung.com.br>
2019-10-21 10:08:25 -0300

    Atualização da página about

--- src/Pages/Home/about/index.js
+++ src/Pages/Home/about/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
 
-import { Container, Text } from './styles';
+import Photo from '../../../Assets/chef.jpg';
+
+import { Container, Content, TextContent, ContainerImage, MainText} from './styles';
 
 const about = () =>
     <Container>
-        <Text>
-            <h1>
-                Nossa Cozinha
-            </h1>
-
-            <div>
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout
-            </div>
-        </Text>
+        <Content>
+            <ContainerImage>
+                <img src ={Photo}/>
+            </ContainerImage>
+            <TextContent>
+                <MainText>
+                    <div>
+                        <h1>Nossa Cozinha</h1>
+                    </div>
+                    <div>
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout
+                    </div>
+                    <div>
+                        Social Media
+                    </div>
+                </MainText>
+            </TextContent>
+        </Content>
     </Container>;
 
 export default about;
--- src/Pages/Home/about/styles.js
+++ src/Pages/Home/about/styles.js
@@ -4,25 +4,43 @@ export const Container = styled.div`
     width: 100%;
     height: 530px;
     background-color: var(--light-1);
+    display: flex;
+    justify-content: center;
 `;
 
-export const Text = styled.div`
+export const Content = styled.div`
+    width: 860px;
+    height: 460px;
+    border: 1px solid grey;
+    display: flex;
+    flex-direction: row;
+    padding-top: 35px;
+`;
 
-    color: var(--light-2);
-    width: 480px;
-    height: 310px;
+export const TextContent = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+export const ContainerImage = styled.div `
+    img {
+        width: 300px;
+        height: 430px;
+        border-radius: 20px;
+    }
+`;
+
+export const MainText = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     background-color: blue;
     
+
     h1 {
         font-size: 36px;
-        text-transform: uppercase;
         font-weight: bold;
+        text-transform: uppercase;
         color: var(--text-green);
-        background-color: red;
     }
-
-    div {
-        background-color: green;
-    }
-    
 `;
